e726ef4a6e5a6a6127ddbc34b8b363f772e28413
Daniel <dharnden@bucs.fsw.edu>
2023-04-30 20:12:45 -0500

    Update generateGraph.py

--- prometheusMain/generateGraph.py
+++ prometheusMain/generateGraph.py
@@ -12,6 +12,7 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
     # Creates the graphviz object that will hold the graph
     dot = graphviz.Digraph()
 
+    # Sorts the parsed text from biggest to smallest table
     parsedText = sorted(parsedText, key=lambda x: len(x))
     parsedText.reverse()
 
@@ -22,76 +23,64 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
         
         nodeInfo = '''<\n\t<table border="1" cellborder="1" cellspacing="0" color="#973835">'''
 
-        # Iterates through each table from the original txt of the inputted database
-        for i, key in enumerate(tableList):
+        # Generates the table using the table name
+        nodeInfo += generateTable(tableList[0], tableNum)
+        tableNum += 1
 
-            if i == 0:
-                # Generates the table using the table name
-                nodeInfo += generateTable(key, tableNum)
-                tableNum += 1
-            else:
-                # Determines if they key has to be renamed based on the mapping
-                # Iterates through each key...
-                for keySynonyms in jsonKeyList:
-                    # ...stores the values (a list of strings)...
-                    values = jsonKeyList[keySynonyms]
-                    # ...and compares the current key to the values. If the key is a synonym...
-                    if key in values:
-                        # ...the current key is replaced with the synonym (the key from the table)
-                        key = keySynonyms
+        # Iterates through each table from the original text of the inputted database
+        for key in tableList[1:]:
+            # Determines if they key has to be renamed based on the mapping
+            # Iterates through each key...
+            for keySynonyms in jsonKeyList:
+                # ...compares the current key to the values. If the key is a synonym...
+                if key in jsonKeyList[keySynonyms]:
+                    # ...the current key is replaced with the synonym (the key from the table)
+                    key = keySynonyms
 
-                # Prevents duplicate keys
-                if key not in tempKeys:
-                    tempKeys.add(key)
-                    # Adds every key as a varchar(50)
-                    # TODO: Use VARCHAR(50) as a fallback and impliment type stealing (when applicable)
-                    nodeInfo += generateKey(key, "VARCHAR(50)")
+            # Prevents duplicate keys
+            if key not in tempKeys:
+                tempKeys.add(key)
+                # Adds every key as a varchar(50)
+                # TODO: Use VARCHAR(50) as a fallback and impliment type stealing (when applicable)
+                nodeInfo += generateKey(key, "VARCHAR(50)")
 
         # Finishes the table then adds the node using the temporary information
         nodeInfo += "\n</table>\n>"
         dot.node(tableList[0], shape='none', label=nodeInfo)
 
-    # Iterates through each key (again)
-    for tableList in parsedText:
-        # The name of the table is stored
-        table = tableList[0]
-        foreignKeysRemaining = foreignKeyNum
+    # Created a nested function since reverse parsing changes very little code
+    def generateForeignKeys(parsedText, reverse):
+        parsedText = list(reversed(parsedText)) if reverse else parsedText
+        print(parsedText)
 
-        for key in tableList[1:]:
-            # Checks to see if this key has appeared before in the new database. If not, it is set as the primary key of the new database, and any subsequent mentions of the key will reference the key/table it first appears
-            values = jsonKeyList[keySynonyms]
-            if key in values:
-                key = keySynonyms
+        # Iterates through each key (again)
+        for tableList in parsedText:
 
-            if key in primaryKeys:
-                # Checks to see if there are any foreign key additions left for this table and checks to ensure the table is not referencing itself
-                if foreignKeysRemaining > 0 and key not in bannedWords:
-                    temp = [primaryKeys[key], table, key]
-                    if temp not in edgesToAdd:
-                        edgesToAdd.append([primaryKeys[key], table, key])
-                        foreignKeysRemaining = foreignKeysRemaining - 1
-            else:
-                primaryKeys[key] = table
-
-    if allowReverseParsing is True:
-        for tableList in reversed(list(parsedText)):
+            # The name of the table is stored
             table = tableList[0]
+            tableList = list(reversed(tableList)) if reverse else tableList
+            print(tableList)
             foreignKeysRemaining = foreignKeyNum
 
-            for key in reversed(list(tableList)):
-                values = jsonKeyList[keySynonyms]
-                if key in values:
+            for key in tableList:
+                # Checks to see if this key has appeared before in the new database. If not, it is set as the primary key of the new database, and any subsequent mentions of the key will reference the key/table it first appears
+                if key in jsonKeyList[keySynonyms]:
                     key = keySynonyms
 
                 if key in primaryKeys and table is not primaryKeys[key]:
+                    # Checks to see if there are any foreign key additions left for this table and checks to ensure the table is not referencing itself
                     if foreignKeysRemaining > 0 and key not in bannedWords:
                         temp = [primaryKeys[key], table, key]
                         if temp not in edgesToAdd:
-                            edgesToAdd.append([primaryKeys[key], table, key])
+                            edgesToAdd.append(temp)
                             foreignKeysRemaining = foreignKeysRemaining - 1
                 else:
                     primaryKeys[key] = table
     
+    # Calls the function
+    generateForeignKeys(parsedText, False)
+    generateForeignKeys(parsedText, True) if allowReverseParsing else None
+
     # Now that all of the tables are mapped, foreign keys can added to the graph in the order they were found
     for referencedTable, tableReferencing, key in edgesToAdd:
         if referencedTable != tableReferencing:
