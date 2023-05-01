22f2eafcbc49a6f8860465b1de6a6dbb856c318b
Daniel Harnden <103148290+DanielHarnden@users.noreply.github.com>
2023-05-01 18:42:12 -0500

    Improved Python Support

--- prometheusMain/generateGraph.py
+++ prometheusMain/generateGraph.py
@@ -2,10 +2,16 @@ import graphviz, os
 
 def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, bannedWords):
     # Initializes variables
-    foreignKeyNum = 1
     primaryKeys = {}
     edgesToAdd = []
     tableNum = 0
+
+    tableNames = []
+    tempTables = []
+    nodes = []
+
+
+
 
     allowReverseParsing = True if allowReverseParsing == "true" else False
 
@@ -16,17 +22,20 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
     parsedText = sorted(parsedText, key=lambda x: len(x))
     parsedText.reverse()
 
-    # Iterates through all of the tables from the original txt of the inputted database
-    for tableList in parsedText:
-        # A temp list of added keys to avoid duplicates
-        tempKeys = set()
-        
-        nodeInfo = '''<\n\t<table border="1" cellborder="1" cellspacing="0" color="#973835">'''
+    # Adds each table to the database
+    for i, tableList in enumerate(parsedText):
+        tempTables.append('''<\n\t<table border="1" cellborder="1" cellspacing="0" color="#973835">''')
 
         # Generates the table using the table name
-        nodeInfo += generateTable(tableList[0], tableNum)
+        tableNames.append(tableList[0] + " [table]")
+        tempTables[i] += generateTable(tableNames[i], tableNum)
+        primaryKeys[tableNames[i]] = tableNames[i]
         tableNum += 1
 
+    # Iterates through all of the tables from the original txt of the inputted database
+    for i, tableList in enumerate(parsedText):
+        # A temp list of added keys to avoid duplicates
+        tempKeys = set()
         # Iterates through each table from the original text of the inputted database
         for key in tableList[1:]:
             # Determines if they key has to be renamed based on the mapping
@@ -42,25 +51,26 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
                 tempKeys.add(key)
                 # Adds every key as a varchar(50)
                 # TODO: Use VARCHAR(50) as a fallback and impliment type stealing (when applicable)
-                nodeInfo += generateKey(key, "VARCHAR(50)")
+                tempTables[i] += generateKey(key, "VARCHAR(50)")
+                nodes.append(f"{tableNames[i]}:{key}")
 
         # Finishes the table then adds the node using the temporary information
-        nodeInfo += "\n</table>\n>"
-        dot.node(tableList[0], shape='none', label=nodeInfo)
+        tempTables[i] += "\n</table>\n>"
+        dot.node(tableNames[i], shape='none', label=tempTables[i])
 
+    tablesVisited = []
     # Created a nested function since reverse parsing changes very little code
     def generateForeignKeys(parsedText, reverse):
         parsedText = list(reversed(parsedText)) if reverse else parsedText
-        print(parsedText)
 
         # Iterates through each key (again)
-        for tableList in parsedText:
+        for i, tableList in enumerate(parsedText):
+            if not reverse:
+                tablesVisited.append([])
 
             # The name of the table is stored
-            table = tableList[0]
+            table = tableNames[i]
             tableList = list(reversed(tableList)) if reverse else tableList
-            print(tableList)
-            foreignKeysRemaining = foreignKeyNum
 
             for key in tableList:
                 # Checks to see if this key has appeared before in the new database. If not, it is set as the primary key of the new database, and any subsequent mentions of the key will reference the key/table it first appears
@@ -69,22 +79,36 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
 
                 if key in primaryKeys and table is not primaryKeys[key]:
                     # Checks to see if there are any foreign key additions left for this table and checks to ensure the table is not referencing itself
-                    if foreignKeysRemaining > 0 and key not in bannedWords:
-                        temp = [primaryKeys[key], table, key]
+                    if primaryKeys[key] not in tablesVisited[i] and key not in bannedWords:
+
+                        if key + " [table]" in tableNames:
+                            temp = (table, key, primaryKeys[key], primaryKeys[key], True)
+                        else:
+                            temp = (table, key, primaryKeys[key], key, False)
+
                         if temp not in edgesToAdd:
                             edgesToAdd.append(temp)
-                            foreignKeysRemaining = foreignKeysRemaining - 1
+                            tablesVisited[i].append(primaryKeys[key])
+                            
                 else:
                     primaryKeys[key] = table
-    
+
     # Calls the function
     generateForeignKeys(parsedText, False)
-    generateForeignKeys(parsedText, True) if allowReverseParsing else None
+
+    if allowReverseParsing:
+        tablesVisited = list(reversed(tablesVisited))
+        generateForeignKeys(parsedText, True)
 
     # Now that all of the tables are mapped, foreign keys can added to the graph in the order they were found
-    for referencedTable, tableReferencing, key in edgesToAdd:
+    for tableReferencing, keyReferencing, referencedTable, referencedKey, referencingTable in edgesToAdd:
         if referencedTable != tableReferencing:
-            dot.edge(f"{tableReferencing}:{key}.end", f"{referencedTable}:{key}.start", arrowhead='vee', arrowtail='odot', dir='both', label=f"{tableReferencing} ref.  {referencedTable}", style='solid')
+            if f"{tableReferencing}:{keyReferencing}" in nodes:
+                dot.edge(f"{tableReferencing}:{keyReferencing}.end", f"{referencedTable}:{referencedKey}.start", arrowhead='vee', arrowtail='odot', dir='both', label=f"{tableReferencing} ref.  {referencedTable}", style='solid')
+            elif f"{referencedTable}:{keyReferencing}" in nodes and referencingTable:
+                dot.edge(f"{referencedTable}:{keyReferencing}.end", f"{tableReferencing}:{tableReferencing}.start", arrowhead='vee', arrowtail='odot', dir='both', label=f"{referencedTable} ref.  {tableReferencing}", style='solid')
+            else:
+                print("Node does not exist.")
 
     # Does some settings to make it look pwetty
     dot.graph_attr.update({
@@ -102,16 +126,16 @@ def generateGraph(parsedText, jsonKeyList, graphName, allowReverseParsing, banne
 
 def generateTable(tableName, tableNum):
     return f'''
-    <tr>
-        <td colspan='2' bgcolor='#B0413E' align='left'><font color="#FFFFEB"><b><i>{tableName}</i></b></font></td>
-        <td bgcolor='#B0413E' align='right'><font color="#FFFFEB"><b><i>{tableNum}</i></b></font></td>
-    </tr>
+    \t<tr>
+        \t<td colspan='2' bgcolor='#B0413E' port="{tableName}.start" align='left'><font color="#FFFFEB"><b><i>{tableName}</i></b></font></td>
+        \t<td bgcolor='#B0413E' align='right' port="{tableName}.end" ><font color="#FFFFEB"><b><i>{tableNum}</i></b></font></td>
+    \t</tr>
     '''
 
 def generateKey(keyName, varType):
     return f'''
-    <tr>
-        <td colspan='2' bgcolor='#973835' port="{keyName}.start" align='left'><font color="#FFFFEB"><b><i>{keyName}</i></b></font></td>
-        <td bgcolor='#973835' port="{keyName}.end" align='right'><font color="#FFFFEB">{varType}</font></td>
-    </tr>
+    \t<tr>
+        \t<td colspan='2' bgcolor='#973835' port="{keyName}.start" align='left'><font color="#FFFFEB"><b><i>{keyName}</i></b></font></td>
+        \t<td bgcolor='#973835' port="{keyName}.end" align='right'><font color="#FFFFEB">{varType}</font></td>
+    \t</tr>
     '''
